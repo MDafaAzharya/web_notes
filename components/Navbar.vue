04b7b02ce1ea9ed74b27933e00518e85e3de1a00
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-11-30 08:28:08 +0700

    Solved Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,18 +4,17 @@ import "../assets/css/index.css";
 <template>
     <!-- Navbar -->
     <nav
-      class="top-0 left-0 w-full z-10 bg-sky-400 md:flex-row md:flex-nowrap md:justify-start  items-center p-4 shadow-lg hidden md:block"
+      class="top-0 left-0 w-full z-10 bg-sky-400 md:flex-row md:flex-nowrap md:justify-startitems-center p-4 shadow-lg hidden md:block"
     >
       <div
         class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
       >
         <!-- Brand -->
-        <a
+        <p
           class="title text-white text-lg  hidden lg:inline-block font-bold"
-          href="javascript:void(0)"
         >
           Welcome Name
-        </a>
+        </p>
         <!-- Form -->
         <div>
             <NuxtLink to="/dashboard/profile"><img src="../assets/img/profile.png" alt="" width="40"></NuxtLink>
