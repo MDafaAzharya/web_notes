7959757c387079f53ca10320b085cf8c75fdda69
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-11-30 08:20:46 +0700

    Solved CSS Warning

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@ import "../assets/css/index.css";
 <template>
     <!-- Navbar -->
     <nav
-      class="top-0 left-0 w-full z-10 bg-sky-400 md:flex-row md:flex-nowrap md:justify-start md:flex items-center p-4 shadow-lg hidden md:block"
+      class="top-0 left-0 w-full z-10 bg-sky-400 md:flex-row md:flex-nowrap md:justify-start  items-center p-4 shadow-lg hidden md:block"
     >
       <div
         class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
